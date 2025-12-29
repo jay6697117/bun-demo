5295716d8824583fcebc1da640fbf157b6fe0ca9
SteveZhang <871506263@qq.com>
2025-12-29 15:09:05 +0800

    1

--- scripts/create_todo.sh
+++ scripts/create_todo.sh
@@ -1,3 +1,4 @@
+timestamp=$(date +%s)
 curl -X POST http://localhost:3000/todos \
      -H "Content-Type: application/json" \
-     -d '{"title": "学习 Bun 真有趣2"}'
+     -d "{\"title\": \"学习 Bun 真有趣-${timestamp}\"}"
