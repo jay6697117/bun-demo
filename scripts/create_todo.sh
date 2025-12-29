847b8ad9865860d2b5e5276a5ca38868dff8e535
SteveZhang <871506263@qq.com>
2025-12-29 15:01:10 +0800

    2

--- scripts/create_todo.sh
+++ scripts/create_todo.sh
@@ -1,3 +1,3 @@
 curl -X POST http://localhost:3000/todos \
      -H "Content-Type: application/json" \
-     -d '{"title": "学习 Bun 真有趣"}'
+     -d '{"title": "学习 Bun 真有趣2"}'
--- update_todo.sh
+++
@@ -1,1 +0,0 @@
-curl -X PUT http://localhost:3000/todos/1
